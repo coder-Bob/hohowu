57b4cf56ec5031565ac2d2519acfe5a745e620f4
Bob <coderSkin@gmail.com>
2017-10-14 22:08:28 +0800

    更新文件

--- gulpfile.js
+++ gulpfile.js
@@ -11,23 +11,6 @@ var gulp        = require('gulp'), //本地安装gulp所用到的地方
     // server      = tiny(),
     livereload  = require('gulp-livereload');   //livereload
 
-//图片处理
-// gulp.task('images',function(){
-//     var imgDev = './dev/images/*.{png,jpg,gif,ico}',
-//         imgSrc = './src/images/';
-//     gulp.src(imgDev)
-//         .pipe(imagemin({
-//             optimizationLevel: 5, //类型：Number  默认：3  取值范围：0-7（优化等级）
-//             progressive: true,  //类型：Boolean 默认：false 无损压缩jpg图片
-//             interlaced: true, //类型：Boolean 默认：false 隔行扫描gif进行渲染
-//             multipass: true, //类型：Boolean 默认：false 多次优化svg直到完全优化
-//             svgoPlugins: [{removeViewBox: false}],//不要移除svg的viewbox属性
-//             use: [pngquant()] //使用pngquant深度压缩png图片的imagemin插件
-//         }))
-//         .pipe(gulp.dest(imgSrc))
-//         .pipe(livereload());
-// });
-
 
 gulp.task('images',function(){
     var imgDev = './dev/images/*.{png,jpg,gif,ico}',
@@ -59,54 +42,36 @@ gulp.task('css',function(){
 })
 
 
-//require合并
-gulp.task('rjs', function () {
-    gulp.src('./src/js/**/*.js')
-        .pipe(amdOptimize("main", {
-            //require config
-            paths: {
-                    "jquery": "../../libs/jquery/dist/jquery.min",
-                    "jquery.serializeJSON": "../../libs/jquery.serializeJSON/jquery.serializejson.min",
-                    "sug": "src/js/suggestion/suggestion",
-                    "validate": "../util/src/js/util/validate",
-                    "urlParam": "../util/src/js/util/url.param"
-            },
-            shim: {
-                "jquery.serializeJSON": ['jquery']
-            }
-    }))
-    .pipe(concat("index.js"))           //合并
-        .pipe(gulp.dest("dist/js"))          //输出保存
-        .pipe(rename("index.min.js"))          //重命名
-        .pipe(uglify())                        //压缩
-        .pipe(gulp.dest("dist/js"));         //输出保存
-    });
 
+//字体输出
+gulp.task('fonts',function(){
+    var fontsDev = './dev/fonts/*',
+        fontsSrc = './src/fonts/';
+    gulp.src(fontsDev)
+        .pipe(gulp.dest(fontsSrc))
+        .pipe(livereload());
+})
 
 
 // HTML处理
 gulp.task('html', function() {
     var htmlSrc = './dev/*.html',
         htmlDst = './src/';
-
     gulp.src(htmlSrc)
         .pipe(gulp.dest(htmlDst))
         .pipe(livereload());
 });
 
 
-
-
-
 //清空图片 样式
 gulp.task('clean',function(){
-    gulp.src(['./src/css','./src/images','./src/*.html'],{read: false})
+    gulp.src(['./src'],{read: false})
         .pipe(clean());
 })
 
 // 默认任务 清空图片、样式 gulp
 gulp.task('default', ['clean'], function(){
-    gulp.start('html','css','images');
+    gulp.start('html','css','fonts','images');
 });
 
 //默认任务
@@ -121,6 +86,11 @@ gulp.task('watch', function () {
         //监听css
         gulp.watch('./dev/sass/*',function () {
             gulp.run('css');
+        })
+
+        //监听字体文件
+        gulp.watch('./dev/fonts/*',function () {
+            gulp.run('fonts');
         })
 
         // 监听html
